dba187c3a0cca034842c0145a406a5e4813e5e1b
johnny_shaman <johnny_shaman@live.jp>
2019-03-10 06:52:58 +0000

    modified:   package.json 	modified:   pvp.js

--- pvp.js
+++ pvp.js
@@ -1,23 +1,24 @@
 (() => {
   'use strict';
-  const _ = require('cte');
+  const _ = require("losand");
   const {offing, ansing} = {offing: _([]), ansing: _([])};
   module.exports = (dir, file, port, ip) => _(require('http').createServer(
     _(require('express'))
     .endo($ => ({
       app: $(),
-      static: $.static
+      "static": $.static
     }))
     .endo($ => _($.app).been
-      .use(require('cors')())
-      .use($.static(dir))
-      .set('view options', {layout: false})
-      .get('/', (req, res) => res.status(200).render(file))
-      ._
+        .use(require('cors')())
+        .use($.static(dir))
+        .set('view options', {layout: false})
+        .get('/', (req, res) => res.status(200).render(file))
+        ._
+      )._
     )._
-  ))
-  .use(sv => _(new (require('ws').Server)({server : sv})).been
-    .on('connection', ws => _(
+  )
+  .use(sv => _(new (require('ws').Server)({server : sv})).on(
+    'connection', ws => _(
       offing._.length <= 0
       ? offing.pushR(
         _(ws).been
@@ -27,15 +28,23 @@
       )
       : ansing.pushR(
         _(ws).been
-        .on('message', m => ansing.use(a => (
-          a.splice(a.findIndex(v => v === ws), 1).pop().close(),
-          offing.popL.been.send(m).close()
-        )))
-        .send(_(offing._[0].sdp).json._)
+        .on('message', m => ansing.use(
+            a => (
+              _(a.findIndex(v => v === ws))
+              .endo(i => a.splice(i, 1))
+              .use(s => s[0].close()),
+              offing.use(
+                o => _(o.shift()).been.send(m).close()
+              )
+            )
+          )
+        )
+        .send(offing._[0].sdp)
         ._
-      )).use(a => ws.on('close', m => a.map($ => $.filter(v => v !== ws))))
-    )._
-  )
-  ._
-  .listen(port || process.env.PORT, ip || process.env.IP);
+      )
+    )
+    .use(a => _(ws).on(
+      'close', m => a.map($ => $.filter(v => v !== ws))
+    ))
+  ))._.listen(port || process.env.PORT, ip || process.env.IP);
 })();
